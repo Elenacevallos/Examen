564f6f2735250387d72491495ee03b8072e22501
Elena Rodriguez <princesa030503@gmail.com>
2024-06-07 12:01:03 -0700

    se realizo la clase Rectangulo

---
+++ app/src/androidTest/java/com/example/examen/Rectangulo.java
@@ -0,0 +1,19 @@
+package com.example.examen;
+
+public class Rectangulo {
+    private float base;
+    private float altura;
+
+    public Rectangulo(float base, float altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public float calcularArea() {
+        return base * altura;
+    }
+
+    public float calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+}
